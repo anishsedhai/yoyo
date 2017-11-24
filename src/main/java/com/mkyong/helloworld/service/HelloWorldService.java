e3a319a5ccfae8297ac1f8e5a26529dcc0b5ce9d
sedhai <anish.sedhai@threemonk.com>
2017-11-24 12:14:16 +0545

    first commit

---
+++ src/main/java/com/mkyong/helloworld/service/HelloWorldService.java
@@ -0,0 +1,33 @@
+package com.mkyong.helloworld.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class HelloWorldService {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldService.class);
+
+	public String getDesc() {
+
+		logger.debug("getDesc() is executed!");
+
+		return "our first copied project";
+
+	}
+
+	public String getTitle(String name) {
+
+		logger.debug("getTitle() is executed! $name : {}", name);
+
+		if(StringUtils.isEmpty(name)){
+			return "Welcome To Chakku-Bakku Group";
+		}else{
+			return "Hello this is " + name;
+		}
+		
+	}
+
+}
